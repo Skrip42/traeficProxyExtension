7badc3ad14e08281a7484976168b70bf9d805af1
skrip <skrip42@gmail.com>
2019-08-20 10:57:25 +0700

    working progress

--- background.js
+++ background.js
@@ -1,17 +1,24 @@
-var currentHost = "empty.local";
-var targetUrls = "empty.local";
+var currentHost = "symfony.test.local";
+var targetUrls = "127.0.0.1";
 
 chrome.webRequest.onBeforeSendHeaders.addListener(
     function (details) {
+        console.log(1);
+        console.log(details);
         details.requestHeaders.push({ name: "Host", value: currentHost});
+        console.log(details);
         return {requestHeaders: details.requestHeaders};
     },
-    {urls: ['*://' + targetUrls.replace('http://').replace('https://') + '/*']}, 
+    {urls: ['*://' + targetUrls.replace('http://', '').replace('https://', '') + '/*']}, 
     ["blocking", "requestHeaders"]
 );
 chrome.webRequest.onBeforeSendHeaders.addListener(
     function (details) {
+        console.log(2);
+        console.log(details);
         details.requestHeaders.push({ name: "Host", value: currentHost});
+        details.url = details.url.replace(currentHost, targetUrls.replace('http://', '').replace('https://', ''));
+        console.log(details);
         return {requestHeaders: details.requestHeaders};
     },
     {urls: ['*://' + currentHost + '/*']},
