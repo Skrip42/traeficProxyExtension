b00fc2f7f5aebc445aa82d8ad2d91a722a9c385d
skrip <skrip42@gmail.com>
2019-08-20 19:46:59 +0700

    fix redirect schema

--- background.js
+++ background.js
@@ -1,27 +1,74 @@
-var currentHost = "symfony.test.local";
-var targetUrls = "127.0.0.1";
+//var currentHost = "symfony.test.local";
+//var targetUrls = "31.135.34.71";
 
-chrome.webRequest.onBeforeSendHeaders.addListener(
-    function (details) {
-        console.log(1);
-        console.log(details);
-        details.requestHeaders.push({ name: "Host", value: currentHost});
-        console.log(details);
-        return {requestHeaders: details.requestHeaders};
-    },
-    {urls: ['*://' + targetUrls.replace('http://', '').replace('https://', '') + '/*']}, 
-    ["blocking", "requestHeaders"]
-);
-chrome.webRequest.onBeforeSendHeaders.addListener(
-    function (details) {
-        console.log(2);
-        console.log(details);
-        details.requestHeaders.push({ name: "Host", value: currentHost});
-        details.url = details.url.replace(currentHost, targetUrls.replace('http://', '').replace('https://', ''));
-        console.log(details);
-        return {requestHeaders: details.requestHeaders};
-    },
-    {urls: ['*://' + currentHost + '/*']},
-    ["blocking", "requestHeaders"]
-);
+var activeHost = {
+};
 
+function isHostActive(host) {
+    return typeof activeHost[host] !== 'undefined';
+}
+
+function addHost(fromUrl, toUrl) {
+    console.log('add host ' + fromUrl);
+    activeHost[fromUrl] = new hostInstance(fromUrl, toUrl);
+    activeHost[fromUrl].appendListeners();
+}
+
+function removeHost(fromUrl) {
+   console.log('remove host ' + fromUrl);
+   activeHost[fromUrl].removeListeners();
+   delete activeHost[fromUrl];
+}
+
+var hostInstance = function (fromUrl, toUrl) {
+    this.fromUrl = fromUrl;
+    this.toUrl = toUrl;
+    this.replaceUrl = function (details) {
+        console.log(fromUrl, toUrl, details);
+        return {
+            redirectUrl: details.url.replace(fromUrl, toUrl)
+        }
+    };
+    this.replaceHeader = function (details) {
+        console.log(fromUrl, toUrl, details);
+        details.requestHeaders.push({ name: "Host", value: fromUrl});
+        return {requestHeaders: details.requestHeaders};
+    };
+    this.appendListeners = function () {
+        chrome.webRequest.onBeforeRequest.addListener(
+            this.replaceUrl,
+            {urls: ['*://' + this.fromUrl + '/*']},
+            ["blocking"]
+        );
+        chrome.webRequest.onBeforeSendHeaders.addListener(
+            this.replaceHeader,
+            {urls: ['*://' + this.toUrl + '/*']},
+            ["blocking", "requestHeaders" ]
+        );
+    };
+    this.removeListeners = function () {
+        chrome.webRequest.onBeforeRequest.removeListener(this.replaceUrl);
+        chrome.webRequest.onBeforeSendHeaders.removeListener(this.replaceHeader);
+    };
+}
+
+//chrome.webRequest.onBeforeRequest.addListener(
+        //function (details) {
+            //console.log(details);
+            //return {
+                //redirectUrl: details.url.replace(currentHost, targetUrls)
+            //}
+        //},
+        //{ urls: ['*://' + currentHost + '/*']},
+        //["blocking"]
+    //);
+
+//chrome.webRequest.onBeforeSendHeaders.addListener(
+    //function (details) {
+        //console.log(details);
+        //details.requestHeaders.push({ name: "Host", value: currentHost});
+        //return {requestHeaders: details.requestHeaders};
+    //},
+    //{urls: ['*://' + targetUrls + '/*']}, 
+    //["blocking", "requestHeaders"]
+//);
--- index.js
+++ index.js
@@ -16,13 +16,13 @@ function eventInit() {
     document.getElementById('traeficHostAccept').addEventListener(
         'click', 
         function () {
-            console.log(document.getElementById('traeficHostInput').value);
+            //console.log(document.getElementById('traeficHostInput').value);
             traeficInstanceList.add(
                 document.getElementById('traeficHostInput').value
             );    
             traeficInstanceList.save(function () {
                 traeficInstanceList.getListWithHost(function (result) {
-                    console.log(result);
+                    //console.log(result);
                     drowRouts(result);
                 });
             }); 
@@ -39,27 +39,43 @@ function eventInit() {
             } else {
                 return;
             }
-            var currentElement = document.getElementsByClassName('active')[0];
-            if (typeof currentElement !== 'undefined') {
-                currentElement.classList.remove('active');
+            //var currentElement = document.getElementsByClassName('active')[0];
+            //if (typeof currentElement !== 'undefined') {
+                //currentElement.classList.remove('active');
+            //}
+            if (route.classList.contains('active')) {
+                route.classList.remove('active');
+                console.log('disable: ' + route.getAttribute('data-rout'));
+                chrome.extension.getBackgroundPage().removeHost(route.getAttribute('data-rout'));
+            } else {
+                route.classList.add('active');
+                console.log('enabled: ' + route.getAttribute('data-rout'));
+                chrome.extension.getBackgroundPage().addHost(
+                    route.getAttribute('data-rout'),
+                    route.getAttribute('data-host')
+                );
             }
-            chrome.extension.getBackgroundPage().targetUrls 
-                = route.getAttribute('data-host');
-            chrome.extension.getBackgroundPage().currentHost
-                = route.getAttribute('data-rout');
-            route.classList.add('active');
+            //chrome.extension.getBackgroundPage().targetUrls 
+                //= route.getAttribute('data-host');
+            //chrome.extension.getBackgroundPage().currentHost
+                //= route.getAttribute('data-rout');
+            //route.classList.add('active');
         }
     )
 }
 
 function drowRouts(traeficHosts) {
     var ret = ''
-    var currentHost = chrome.extension.getBackgroundPage().currentHost;
-    var currentTraeficHost = chrome.extension.getBackgroundPage().targetUrls;
-    console.log(currentHost);
-    console.log(currentTraeficHost);
+    //var currentHost = chrome.extension.getBackgroundPage().currentHost;
+    //var currentTraeficHost = chrome.extension.getBackgroundPage().targetUrls;
+    //console.log(currentHost);
+    //console.log(currentTraeficHost);
     for (var traeficHost in traeficHosts) {
         var tHosts = traeficHosts[traeficHost]
+        var sHost = traeficHost.replace('https://', '');
+        var sHost = sHost.replace('http://', '');
+        //tHosts = tHosts.replace('https://', '');
+        //tHosts = tHosts.replace('http://', '');
         ret += '<div data-host="' + traeficHost + '">'
         ret += '<div class="traeficHostTitle">'
         ret += '<span><a href="' + traeficHost + '">' + traeficHost + '</a></span>'
@@ -68,12 +84,11 @@ function drowRouts(traeficHosts) {
         for (var i = 0; i < tHosts.length; i++) {
             ret += '<div class="route';
             if (
-                traeficHost == currentTraeficHost
-                && tHosts[i] == currentHost
+                chrome.extension.getBackgroundPage().isHostActive(tHosts[i])
             ) {
                 ret += ' active'
             }
-            ret += '" data-host="' + traeficHost 
+            ret += '" data-host="' + sHost
                 + '" data-rout="' + tHosts[i] + '">'
             ret += '<span class="activeIcon"><i class="material-icons">check_circle</i></span>'
             ret += '<span>' + tHosts[i] + '</span>'
